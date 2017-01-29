4cfa3992d79b114b6579aff4ff71f2337b06205c
Marcio Vieira <marcio.vieira@mttnow.com>
2017-01-29 13:51:33 +0000

    no message

--- src/main/java/com/mttnow/session/CustomCacheConfig.java
+++ src/main/java/com/mttnow/session/CustomCacheConfig.java
@@ -1,72 +1,81 @@
 package com.mttnow.session;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.redisson.Redisson;
-import org.redisson.api.RedissonClient;
-import org.redisson.config.Config;
-import org.redisson.spring.cache.CacheConfig;
-import org.redisson.spring.cache.RedissonSpringCacheManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
-import org.springframework.cache.annotation.CachingConfigurer;
+import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.cache.interceptor.KeyGenerator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @Configuration
-//@EnableCaching
-public class CustomCacheConfig  implements CachingConfigurer {
+@EnableCaching
+public class CustomCacheConfig extends CachingConfigurerSupport {
 
 	private static final Logger LOG = LoggerFactory.getLogger(CustomCacheConfig.class);
 	private static final Long ONE_SECOND = 60 * 1000L;
-	
-	@Value("${custom.spring.cache.address}")
-	private String cacheAddress;
 
-	private RedissonClient redisson() {
-		LOG.info("\n\n\n*********************");
-		LOG.info("CacheAddress: " + cacheAddress);
-		LOG.info("*********************\n\n\n\n");
-		Config config = new Config();
-		config.useSingleServer().setAddress(cacheAddress);
-		return Redisson.create(config);
+	@Value("${custom.spring.cache.hostName}")
+	private String cacheHostName;
+
+	@Value("${custom.spring.cache.port}")
+	private int cachePort;
+
+	@Bean
+	public JedisConnectionFactory redisConnectionFactory() {
+		JedisConnectionFactory redisConnectionFactory = new JedisConnectionFactory();
+
+		redisConnectionFactory.setHostName(cacheHostName);
+		redisConnectionFactory.setPort(cachePort);
+		return redisConnectionFactory;
 	}
 
 	@Bean
-	@Override
-	public CacheManager cacheManager() {
-		Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
-
-		config.put("beerResult", new CacheConfig(30 * ONE_SECOND, 60 * ONE_SECOND));
-
-		RedissonSpringCacheManager redissonSpringCacheManager = new RedissonSpringCacheManager(redisson(), config);
-
-		return redissonSpringCacheManager;
+	public RedisTemplate<String, String> redisTemplate(RedisConnectionFactory cf) {
+		RedisTemplate<String, String> redisTemplate = new RedisTemplate<String, String>();
+		redisTemplate.setConnectionFactory(cf);
+		return redisTemplate;
 	}
 
-	@Override
-	public CacheResolver cacheResolver() {
-		// TODO Auto-generated method stub
-		return null;
+	@Bean
+	public CacheManager cacheManager(RedisTemplate redisTemplate) {
+		RedisCacheManager cacheManager = new RedisCacheManager(redisTemplate);
+		cacheManager.setDefaultExpiration(60 * ONE_SECOND);
+		return cacheManager;
 	}
 
-	@Override
-	public KeyGenerator keyGenerator() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+	//
+	//
+	// private RedissonClient redisson() {
+	// LOG.info("\n\n\n*********************");
+	// LOG.info("CacheAddress: " + cacheAddress);
+	// LOG.info("*********************\n\n\n\n");
+	// Config config = new Config();
+	// config.useSingleServer().setAddress(cacheAddress);
+	// return Redisson.create(config);
+	// }
+	//
+	// @Bean
+	// @Override
+	// public CacheManager cacheManager() {
+	// Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
+	//
+	// config.put("beerResult", new CacheConfig(30 * ONE_SECOND, 60 *
+	// ONE_SECOND));
+	//
+	// RedissonSpringCacheManager redissonSpringCacheManager = new
+	// RedissonSpringCacheManager(redisson(), config);
+	//
+	// return redissonSpringCacheManager;
+	// }
 
-	@Override
-	public CacheErrorHandler errorHandler() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 
 }
--- src/main/java/com/mttnow/session/SpringBootDemoApplication.java
+++ src/main/java/com/mttnow/session/SpringBootDemoApplication.java
@@ -2,7 +2,8 @@ package com.mttnow.session;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.CachingConfigurerSupport;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
@@ -13,53 +14,26 @@ import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@EnableCaching
 @EnableSwagger2
 @SpringBootApplication
-public class SpringBootDemoApplication extends CachingConfigurerSupport {
-
+public class SpringBootDemoApplication {
 	
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
 	}
 
 	@Bean
-	RestTemplate restTemplate() {
+	public RestTemplate restTemplate() {
 		return new RestTemplate();
 	}
 
 	@Bean
-	Docket newsApi() {
-		return new Docket(DocumentationType.SWAGGER_2).groupName("Beer").apiInfo(apiInfo()).select()
-				.paths(PathSelectors.regex("/.*")).build();
+	public Docket newsApi() {
+		return new Docket(DocumentationType.SWAGGER_2).groupName("Beer").apiInfo(apiInfo()).select().paths(PathSelectors.regex("/.*")).build();
 	}
 
-	ApiInfo apiInfo() {
-		return new ApiInfoBuilder().title("Spring REST Application").description("Spring REST Application")
-				.contact("Marcio Vieira").license("Apache License Version 2.0").version("2.0").build();
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Spring REST Application").description("Spring REST Application").contact("Marcio Vieira").license("Apache License Version 2.0").version("2.0").build();
 	}
-
-	/*
-	 * JEDIS CONNECTION
-	 * 
-	 * @Bean RedisTemplate<Object, Object> redisTemplate() {
-	 * RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<Object,
-	 * Object>(); redisTemplate.setConnectionFactory(redisson());
-	 * redisTemplate.set return redisTemplate; }
-	 * 
-	 * @Bean JedisConnectionFactory jedisConnectionFactory() {
-	 * JedisConnectionFactory factory = new JedisConnectionFactory();
-	 * factory.setHostName("localhost"); factory.setPort(6379);
-	 * factory.setUsePool(true); return factory; }
-	 * 
-	 * @Bean CacheManager cacheManager() {
-	 * 
-	 * RedissonSpringCacheManager redisCacheManager = new
-	 * RedissonSpringCacheManager(redisTemplate());
-	 * redisCacheManager.setUsePrefix(true);
-	 * redisCacheManager.setDefaultExpiration(60);
-	 * 
-	 * Map<String, Long> expires = new HashMap<>(); expires.put("beerResult",
-	 * 30L); redisCacheManager.setExpires(expires); return redisCacheManager; }
-	 */
-
 }
