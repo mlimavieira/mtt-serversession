798dcabc4bb615fb16d763dc47fc0785e5a754e5
Marcio Vieira <marcio.vieira@mttnow.com>
2017-01-29 13:21:57 +0000

    no message

--- src/main/java/com/mttnow/session/CustomCacheConfig.java
+++ src/main/java/com/mttnow/session/CustomCacheConfig.java
@@ -8,6 +8,8 @@ import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
 import org.redisson.spring.cache.CacheConfig;
 import org.redisson.spring.cache.RedissonSpringCacheManager;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurer;
@@ -15,24 +17,29 @@ import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.cache.interceptor.CacheResolver;
 import org.springframework.cache.interceptor.KeyGenerator;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-@EnableCaching
-public class CustomCacheConfig implements CachingConfigurer {
+@Configuration
+//@EnableCaching
+public class CustomCacheConfig  implements CachingConfigurer {
 
+	private static final Logger LOG = LoggerFactory.getLogger(CustomCacheConfig.class);
 	private static final Long ONE_SECOND = 60 * 1000L;
 	
-	@Value("#{custom.spring.cache.address}")
+	@Value("${custom.spring.cache.address}")
 	private String cacheAddress;
 
 	private RedissonClient redisson() {
-		System.out.println("\n\n\n*********************");
-		System.out.println("CacheAddress: " + cacheAddress);
-		System.out.println("*********************\n\n\n\n");
+		LOG.info("\n\n\n*********************");
+		LOG.info("CacheAddress: " + cacheAddress);
+		LOG.info("*********************\n\n\n\n");
 		Config config = new Config();
 		config.useSingleServer().setAddress(cacheAddress);
 		return Redisson.create(config);
 	}
 
+	@Bean
 	@Override
 	public CacheManager cacheManager() {
 		Map<String, CacheConfig> config = new HashMap<String, CacheConfig>();
